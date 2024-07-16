4016f6be90279b9ecdb898527c420a264e4f350d
Primakov Valeriy <sinekura13@mail.ru>
2024-07-16 22:06:31 +0500

    4. Тесты для стороны прямоугольника. [#159460]

--- src/main/java/ru/job4j/condition/RectangleArea.java
+++ src/main/java/ru/job4j/condition/RectangleArea.java
@@ -12,5 +12,7 @@ public class RectangleArea {
         System.out.println(" p = 4, k = 1, s = 1, real = " + result);
         double result1 = RectangleArea.square(6, 2);
         System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+        double result2 = RectangleArea.square(8, 4);
+        System.out.println("p = 8, k = 4, s = 2.56, real = " + String.format("%.4f", result2));
     }
 }
