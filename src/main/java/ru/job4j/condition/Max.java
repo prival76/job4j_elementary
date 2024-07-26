4dafa061ca6778de2841b79e20e6077c0713e165
prival76 <sinekura13@mail.ru>
2024-07-26 20:29:41 +0500

    4.2. Тернарное сравнение [#189]

---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        int result;
+        result = left >= right ? left : right;
+        return  result;
+    }
+
+    public static void main(String[] args) {
+        int left = 8;
+        int right = 4;
+        int result = Max.max(left, right);
+        System.out.println("Maximum number: " + result);
+    }
+}
