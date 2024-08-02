ad14dc9c62b61f0caf9a747a1179d65cf31ee14f
prival76 <sinekura13@mail.ru>
2024-08-02 09:43:27 +0500

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -9,9 +9,23 @@ public class Counter {
             return sum;
     }
 
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int number = start; number <= finish; number++) {
+            if (number % 2 == 0) {
+                sum = sum + number;
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         System.out.println(sum(0, 10));
-        System.out.println(sum(13, 0));
+        System.out.println(sum(-5, 0));
         System.out.println(sum(1, 1));
+
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(-5, 0));
+        System.out.println(sumByEven(1, 1));
     }
 }
--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -33,4 +33,31 @@ class CounterTest {
         assertThat(result).isEqualTo(expected);
     }
 
+    @Test
+    void whenSumEvenNumberFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumByEvenFromMinusFiveToZero() {
+        int start = -5;
+        int finish = 0;
+        int result = Counter.sumByEven(start, finish);
+        int expected = -6;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumByEvenFromStartLargerToFinish() {
+        int start = 10;
+        int finish = 0;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+
+    }
 }
