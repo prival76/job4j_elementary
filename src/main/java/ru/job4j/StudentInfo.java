f840d708bdec2e3a61a6755c5284ff6e143007b7
prival76 <sinekura13@mail.ru>
2024-08-20 20:19:49 +0500

    4.1. Разбор первой программы. [#159033 #502640]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Valeriy Primakov");
+        System.out.println("01.12.1976");
+    }
+}
+
+
