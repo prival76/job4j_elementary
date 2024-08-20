1da551d0fc86b3244ecb4547c4e6c30ef9b4d2a7
prival76 <sinekura13@mail.ru>
2024-08-20 21:54:12 +0500

    9.1. String [#237407 #504651]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I lake Java!";
+        System.out.println(idea);
+        idea +=  "But I am newbie.";
+        int year = 2024;
+        idea = idea + year;
+        System.out.println(idea);
+    }
+}
+
