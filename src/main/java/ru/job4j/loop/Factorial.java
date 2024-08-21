f0a69c074ac19e50cc98078f1101b23d7b779698
prival76 <sinekura13@mail.ru>
2024-08-21 20:12:11 +0500

    5.2. Создать программу, вычисляющую факториал. [#193 #507561]

---
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    public static int calculate(int number) {
+        int result = 1;
+        for (int i = 2; i <= number; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculate(5));
+    }
+}
+
---
+++ src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class FactorialTest {
+
+    @Test
+    void whenCalculateFactorialForFiveThenOneHundredTwenty() {
+        int expected = 120;
+        int number = 5;
+        int output = Factorial.calculate(number);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenCalculateFactorialForZeroThenOne() {
+        int expected = 1;
+        int number = 0;
+        int output = Factorial.calculate(number);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenCalculateFactorialForOneThenOne() {
+        int expected = 1;
+        int number = 0;
+        int output = Factorial.calculate(number);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenCalculateFactorialForMinusThenNumber() {
+        int number = -1;
+        int expected = 1;
+        int output = Factorial.calculate(number);
+        assertThat(output).isEqualTo(expected);
+    }
+}
