fdec96052bf508e744dbfcb0ee13f7b46ef47eed
prival76 <sinekura13@mail.ru>
2024-08-26 08:48:00 +0500

    5.7. Ипотека [#156315]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,12 +3,7 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double amountOfDebt = (amount + amount * percent / 100);
-        if (salary >= amountOfDebt) {
-            year = 1;
-            return year;
-        }
-        while (amount >= 0) {
+        while (amount > 0) {
             amount = (amount + amount * percent / 100) - salary;
             year += 1;
         }
@@ -16,6 +11,6 @@ public class Mortgage {
     }
 
     public static void main(String[] args) {
-        System.out.println(Mortgage.year(100, 120, 50));
+        System.out.println(Mortgage.year(100, 120, 20));
     }
 }
