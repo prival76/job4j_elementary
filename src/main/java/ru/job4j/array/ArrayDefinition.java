f501e6fbce14aa384b11da6a8eff105026832784
prival76 <sinekura13@mail.ru>
2024-08-26 20:13:49 +0500

    6.0.2. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,7 +3,10 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.println("Размер целочисленного массива ages равен - " + ages.length + " ячеек.");
         String[] surnames = new String[100500];
+        System.out.println("Размер строкового массива surnames равен - " + surnames.length + " ячеек.");
         float[] prices = new float[40];
+        System.out.println("Размер массива prices типа float равен - " + prices.length + " ячейкам.");
     }
 }
