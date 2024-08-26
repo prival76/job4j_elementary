80299830843ef36c862452d7fdbaea6eb88d1a1a
prival76 <sinekura13@mail.ru>
2024-08-26 20:25:34 +0500

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,5 +1,7 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
@@ -8,5 +10,12 @@ public class ArrayDefinition {
         System.out.println("Размер строкового массива surnames равен - " + surnames.length + " ячеек.");
         float[] prices = new float[40];
         System.out.println("Размер массива prices типа float равен - " + prices.length + " ячейкам.");
+        String[] names = new String[5];
+        names[0] = "Виктор";
+        names[1] = "Полина";
+        names[2] = "Петр";
+        names[3] = "Стас";
+        names[4] = "Валерий";
+        System.out.println(Arrays.toString(names));
     }
 }
