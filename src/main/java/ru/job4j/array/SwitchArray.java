7590383e06f6d4bb67bdbc7dfb31b9c2f54e77b6
prival76 <sinekura13@mail.ru>
2024-08-27 22:27:43 +0500

    6.2.2. Переставить элементы массива [#226795]

--- src/main/java/ru/job4j/array/SwitchArray.java
+++ src/main/java/ru/job4j/array/SwitchArray.java
@@ -1,18 +1,36 @@
 package ru.job4j.array;
 
 public class SwitchArray {
+    public static int[] swap(int[] array, int source, int destinition) {
+        int temp = array[source];
+        array[source] = array[destinition];
+        array[destinition] = temp;
+        return array;
+    }
+
     public static int[] swapBorder(int[] array) {
         int temp = array[0];
         array[0] = array[array.length - 1];
         array[array.length - 1] = temp;
-        return  array;
+        return array;
     }
 
     public static void main(String[] args) {
         int[] numbers = new int[] {1, 2, 3, 4, 5, 6};
         int[] result = swapBorder(numbers);
         for (int i : result) {
-            System.out.println(i);
+            System.out.print(i);
+        }
+        /* чтобы не было путаницы в восприятии и для большей наглядности
+        при решении задачи использовал новый массив чисел
+        и отделил решения друг от друга переводом строки
+         */
+        System.out.println();
+
+        int[] newNumbers = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] resultSwap = swap(newNumbers, 1, 4);
+        for (int i : resultSwap) {
+            System.out.print(i);
         }
     }
 
--- src/test/java/ru/job4j/array/SwitchArrayTest.java
+++ src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -35,4 +35,34 @@ class SwitchArrayTest {
         int[] expected = {1};
         assertThat(result).containsExactly(expected);
     }
+
+    @Test
+    void whenSwap0to3() {
+        int[] input = {1, 2, 3, 4};
+        int source = 0;
+        int destination = input.length - 1;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {4, 2, 3, 1};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenSwap4to7() {
+        int[] input = {1, 2, 3, 4, 5, 6, 7, 8};
+        int source = 4;
+        int destination = 7;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {1, 2, 3, 4, 8, 6, 7, 5};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenSwap2to5() {
+        int[] input = {1, 2, 3, 4, 5, 6, 7, 8};
+        int source = 2;
+        int destination = 5;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {1, 2, 6, 4, 5, 3, 7, 8};
+        assertThat(result).containsExactly(expected);
+    }
 }
